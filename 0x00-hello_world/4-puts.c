b6f8197094b058a5f388a6c6c186efe3ec3e2f30
root <adrianmuk>
2022-09-08 14:15:26 -0700

    hello_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,11 @@
  * main - entrypoint
  *
  * Return: always 0
+ *
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
