6925a1adf8bf6901e917ab191e761444d166a8c1
root <adrianmuk>
2022-09-15 08:25:02 -0700

    a lot

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: integer
+ * @b: integer
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,10 @@
 #define _MAIN_H_
 
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar (char c);
 
 
 
