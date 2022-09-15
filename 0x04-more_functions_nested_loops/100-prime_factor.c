02428651c68d12867f529604d418e697a2ae3103
root <adrianmuk>
2022-09-15 13:42:44 -0700

    primefactor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int num = 2, maxFact;
+
+	long int n = 612852475143;
+
+	while (n != 0)
+	{
+		if (n % num != 0)
+		{
+			num++;
+		}
+		else
+		{
+			maxFact = n;
+			n = n / num;
+			if (n == 1)
+			{
+				printf("%ld", maxFact);
+				break;
+			}
+		}
+	}
+	printf("\n");
+	return(0);
+}
+
