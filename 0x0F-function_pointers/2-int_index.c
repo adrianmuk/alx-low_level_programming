bec1d7373026f85a9cd423039f28a72f74d9a0e8
root <adrianmuk>
2022-10-12 09:29:05 -0700

    mandatory

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,7 +3,7 @@
 /**
  * int_index - searches for an integer
  * @array: array of integers
- * size: number of elements in the array
+ * @size: number of elements in the array
  * @cmp: pointer to the function used to compare values
  * Return: index of the first element for which the cmp function doesnt
  * return 0
